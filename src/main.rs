e84404fdae674c9febce5241ccdfd83b0fefc7da
Tummetott <19708025+Tummetott@users.noreply.github.com>
2023-04-11 20:35:45 +0200

    added first draft of lazyString

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::string_library::LazyStr;
 use crate::vector_algebra::Vec3;
 use crate::vector_algebra::Vec3Expr;
 
@@ -11,9 +12,18 @@ fn main() {
     //let vx = va + vb + vc;
     //let vy = va - vb - vc;
     let vz = va * vb;
-    let vzz = vc * 3.5;
+    // let vzz = vc * 3.5;
+    let vzz = 3.5 * vc * 2.0;
     let z = Vec3::from(vz);
     let zz = Vec3::from(vzz);
     println!("z = ({}, {}, {})", z.x(), z.y(), z.z());
     println!("zz = ({}, {}, {})", zz.x(), zz.y(), zz.z());
+
+
+
+    let a = LazyStr::new("foo", "bar");
+    // let b = LazyStr::new("bazz", "bizz");
+    let r = a + "bazz" + "bizz";
+
+    println!("{}", r.to_string());
 }
--- src/string_library.rs
+++ src/string_library.rs
@@ -1,1 +1,58 @@
-// TODO: String library
+#![allow(dead_code)]
+
+use std::fmt::{Display, Formatter, Result};
+use std::ops::Add;
+
+pub struct LazyStr<A, B>
+where
+    A: Display,
+    B: Display,
+{
+    a: A,
+    b: B,
+}
+
+// Constructor
+impl<A, B> LazyStr<A, B> 
+where
+    A: Display,
+    B: Display,
+{
+    pub fn new(a: A, b: B) -> LazyStr<A, B> {
+        LazyStr { a, b }
+    }
+}
+
+// When implementing the Display trait, the .to_string method can be called on the struct
+impl<A, B> Display for LazyStr<A, B> 
+where
+    A: Display,
+    B: Display,
+{
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        // We can just call fmt() on each struct member. Since we use the same formatter, both
+        // strings are written after another
+        self.a.fmt(f)?;
+        self.b.fmt(f)?;
+        Ok(())
+    }
+}
+
+#[test]
+fn string_test() {
+    let a = LazyStr::new("foo", "bar");
+    assert_eq!("foobar", a.to_string());
+}
+
+// Implementing the add trait let's us use the '+' operator
+impl<A, B, C> Add<C> for LazyStr<A, B>
+where
+    A: Display,
+    B: Display,
+    C: Display,
+{
+    type Output = LazyStr<LazyStr<A, B>, C>;
+    fn add(self, other: C) -> Self::Output {
+        LazyStr::new(self, other)
+    }
+}
