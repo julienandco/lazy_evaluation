cf5692a60f549e34e60316108e7d20b7de05bb97
Tummetott <19708025+Tummetott@users.noreply.github.com>
2023-04-12 13:46:51 +0200

    better version of string lib

--- src/string_library.rs
+++ src/string_library.rs
@@ -1,34 +1,33 @@
-#![allow(dead_code)]
-
 use std::fmt::{Display, Formatter, Result};
 use std::ops::Add;
 
-pub struct LazyStr<A, B>
-where
-    A: Display,
-    B: Display,
-{
+pub struct LazyStr<T: Display>(T);
+
+pub struct StrNode<A: Display, B: Display> {
     a: A,
     b: B,
 }
 
 // Constructor
-impl<A, B> LazyStr<A, B> 
-where
-    A: Display,
-    B: Display,
-{
-    pub fn new(a: A, b: B) -> LazyStr<A, B> {
-        LazyStr { a, b }
+impl<T: Display> LazyStr<T> {
+    pub fn new(t: T) -> Self {
+        Self(t)
     }
 }
 
-// When implementing the Display trait, the .to_string method can be called on the struct
-impl<A, B> Display for LazyStr<A, B> 
-where
-    A: Display,
-    B: Display,
-{
+// When implementing the Display trait, the .to_string() method can be called on the struct
+impl<T: Display> Display for LazyStr<T> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        // For our container node, we just call fmt() on the generic type
+        self.0.fmt(f)?;
+        Ok(())
+    }
+}
+
+// We must also implement the Display trait for the inner nodes
+impl<A: Display, B: Display> Display for StrNode<A, B> {
+    #[inline]
     fn fmt(&self, f: &mut Formatter) -> Result {
         // We can just call fmt() on each struct member. Since we use the same formatter, both
         // strings are written after another
@@ -38,21 +37,20 @@ where
     }
 }
 
-#[test]
-fn string_test() {
-    let a = LazyStr::new("foo", "bar");
-    assert_eq!("foobar", a.to_string());
-}
-
 // Implementing the add trait let's us use the '+' operator
-impl<A, B, C> Add<C> for LazyStr<A, B>
-where
-    A: Display,
-    B: Display,
-    C: Display,
-{
-    type Output = LazyStr<LazyStr<A, B>, C>;
-    fn add(self, other: C) -> Self::Output {
-        LazyStr::new(self, other)
+impl<S: Display, T: Display> Add<S> for LazyStr<T> {
+    type Output = LazyStr<StrNode<T, S>>;
+    #[inline]
+    fn add(self, other: S) -> Self::Output {
+        LazyStr(StrNode {
+            a: self.0,
+            b: other,
+        })
     }
 }
+
+// #[test]
+// fn string_test() {
+//     let a = LazyStr::new("foo", "bar");
+//     assert_eq!("foobar", a.to_string());
+// }
